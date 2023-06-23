b21e717ccbe9af6da8e7d07f4babd03d158f5162
alexdionanta <128551228+morenodionanta@users.noreply.github.com>
2023-06-23 19:55:17 -0300

    CRIANDO A BARRA DE MENU

--- src/main/java/com/mycompany/testemenu/view/menu.java
+++ src/main/java/com/mycompany/testemenu/view/menu.java
@@ -26,21 +26,69 @@ public class menu extends javax.swing.JFrame {
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
 
+                jMenuBar1 = new javax.swing.JMenuBar();
+                jMenu1 = new javax.swing.JMenu();
+                jMenu2 = new javax.swing.JMenu();
+                jMenuBar2 = new javax.swing.JMenuBar();
+                ARQUIVO = new javax.swing.JMenu();
+                jMenuItem1 = new javax.swing.JMenuItem();
+                jMenuItem2 = new javax.swing.JMenuItem();
+                jMenu4 = new javax.swing.JMenu();
+
+                jMenu1.setText("File");
+                jMenuBar1.add(jMenu1);
+
+                jMenu2.setText("Edit");
+                jMenuBar1.add(jMenu2);
+
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+                ARQUIVO.setText("ARQUIVO");
+
+                jMenuItem1.setText("ABRIR");
+                jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jMenuItem1ActionPerformed(evt);
+                        }
+                });
+                ARQUIVO.add(jMenuItem1);
+
+                jMenuItem2.setText("BOTÃO RADIO");
+                jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+                        public void actionPerformed(java.awt.event.ActionEvent evt) {
+                                jMenuItem2ActionPerformed(evt);
+                        }
+                });
+                ARQUIVO.add(jMenuItem2);
+
+                jMenuBar2.add(ARQUIVO);
+
+                jMenu4.setText("EDITAR");
+                jMenuBar2.add(jMenu4);
+
+                setJMenuBar(jMenuBar2);
 
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
                 getContentPane().setLayout(layout);
                 layout.setHorizontalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 400, Short.MAX_VALUE)
+                        .addGap(0, 511, Short.MAX_VALUE)
                 );
                 layout.setVerticalGroup(
                         layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
+                        .addGap(0, 316, Short.MAX_VALUE)
                 );
 
                 pack();
         }// </editor-fold>//GEN-END:initComponents
+
+        private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+                // TODO add your handling code here:
+        }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+        private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
+                // TODO add your handling code here:
+        }//GEN-LAST:event_jMenuItem2ActionPerformed
 
 	/**
 	 * @param args the command line arguments
@@ -78,5 +126,13 @@ public class menu extends javax.swing.JFrame {
 	}
 
         // Variables declaration - do not modify//GEN-BEGIN:variables
+        private javax.swing.JMenu ARQUIVO;
+        private javax.swing.JMenu jMenu1;
+        private javax.swing.JMenu jMenu2;
+        private javax.swing.JMenu jMenu4;
+        private javax.swing.JMenuBar jMenuBar1;
+        private javax.swing.JMenuBar jMenuBar2;
+        private javax.swing.JMenuItem jMenuItem1;
+        private javax.swing.JMenuItem jMenuItem2;
         // End of variables declaration//GEN-END:variables
 }
